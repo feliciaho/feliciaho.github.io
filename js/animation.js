8cc88e1c936c8c46e7a3b2d6db7cc2157e592d2c
unknown <felicia18felicia18@gmail.com>
2025-04-19 00:48:23 +0800

    fix navBar

--- js/animation.js
+++ js/animation.js
@@ -1,35 +1,45 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.set(".secTitle", { scale: 0.5 });
-gsap.set(".title2", { scale: 0.5 });
-// gsap.set(".secText", { yPercent: -10});
+gsap.set(".project_s1", { yPercent: -10});
 gsap.set(".banner", { yPercent: 10 });
 
+// 一開始就動
 gsap.to(".mainTitle1", {
   xPercent: 200,
   scale: 5,
   opacity: 0,
   ease: "none",
-  start: "top center",
-  end: "bottom center",
   scrollTrigger: {
     scrub: 1,
   },
 });
 
+// 當滾動到特定區域時，觸發動畫
 document.querySelectorAll(".secTitle").forEach((secTitle) => {
   gsap.to(secTitle, {
     yPercent: 5,
     scale: 1.2,
     ease: "none",
-    start: "top center",
-    end: "bottom center",
     scrollTrigger: {
       trigger: secTitle,
       scrub: 1,
     },
   });
 });
+
+document.querySelectorAll(".project_s1").forEach((project_s1) => {
+  gsap.to(project_s1, {
+    yPercent: 0,
+    ease: "none",
+    scale: 1,
+    scrollTrigger: {
+      trigger: project_s1,
+      scrub: 1,
+    },
+  });
+});
+
 
 document.querySelectorAll(".banner").forEach((banner) => {
   gsap.to(banner, {
--- js/common.js
+++ js/common.js
@@ -6,53 +6,63 @@ const App = {
         {
           type: "Frontend designer",
           name: "Happytuk",
-          time: "April 30, 2024~present",
+          time: "April, 2024~April, 2025",
           skills: ["Pug", "SASS", "jQuery", "Gulp", "CICD", "Vue", "Git"],
           list:[
             "負責開發並維護官網及活動頁，使用 Pug、SASS 和 JavaScript，jQuery 實現響應式設計和互動功能。",
             "使用 Vue 框架，協助公司將遊戲活動頁面從 JavaScript、jQuery 過渡到 Vue 開發模式。",
             "使用 Gulp 進行前端自動化工具，提升開發效率。",
             "使用 Git 進行版本控制，並進行 CICD 部署。",
+            "視覺設計：負責製作網頁主視覺、遊戲 banner。",
           ],
         },
         {
           type: "Website Designer",
           name: "Digeam",
-          time: "April 11, 2023~April 29, 2024",
+          time: "April, 2023~April, 2024",
           skills: ["HTML", "SCSS", "jQuery", "javascript", "RESTful API", "Vue", "Git"],
           list:[
             "負責開發並維護官網及活動頁，使用 HTML、SCSS 和 JavaScript，jQuery 實現響應式設計和互動功能。",
             "引入並整合 Vue 框架，協助公司從 JavaScript、jQuery 過渡到 Vue 開發模式。",
             "RESTful API 串接，實現與後端系統的數據交互和通知。",
-            "平面設計：負責製作網頁主視覺、遊戲 banner。",
+            "視覺設計：負責製作網頁主視覺、遊戲 banner。",
           ],
         },
       ],
       companyProjects: [
-        {
-          type: "EOV",
-          name: "official website",
-          skills: ["Pug", "SASS", "jQuery", "Gulp", "CICD"],
-          webUrl: "https://eov.mangot5.com/eov/index",
-          githubUrl: "https://github.com/feliciaho/eov",
-          img : "/images/eovBanner.png",
-        },
-        {
-          type: "Ghost",
-          name: "Christmas website",
-          skills: ["HTML", "SCSS", "jQuery" ,"RESTful API"],
-          webUrl: "https://go.digeam.com/event/20231220/",
-          githubUrl: "https://github.com/feliciaho/ghost_20231220",
-          img : "/images/ghostBanner.png",
-        },
         {
           type: "Cabel",
           name: "Activity website",
           skills: ["HTML", "SASS", "javascript", "Vue" ,"RESTful API"],
           webUrl: "https://feliciaho.github.io/cbo_20240129",
           githubUrl: "https://github.com/feliciaho/cbo_20240129",
-          img : "/images/cboBanner.png",
+          img : "images/cboBanner.png",
         },
+        {
+          type: "Ghost",
+          name: "Christmas website",
+          skills: ["HTML", "SCSS", "jQuery" ,"RESTful API"],
+          webUrl: "https://feliciaho.github.io/ghost_20231220",
+          githubUrl: "https://github.com/feliciaho/ghost_20231220",
+          img : "images/ghostBanner.png",
+        },
+        {
+          type: "EOV",
+          name: "official website",
+          skills: ["Pug", "SASS", "jQuery", "Gulp", "CICD"],
+          webUrl: "https://eov.mangot5.com/eov/index",
+          githubUrl: "https://github.com/feliciaho/eov",
+          img : "images/eovBanner.png",
+        },
+        {
+          type: "LostArk",
+          name: "Info website",
+          skills: ["Pug", "SASS", "jQuery","Vue", "Gulp", "CICD"],
+          webUrl: "https://landing.mangot5.com/template/lostark/event/250327_pletze/index.html",
+          githubUrl: "https://github.com/feliciaho/250327_pletze",
+          img : "images/lostarkBanner.jpg",
+        },
+
       ],
       sideProjects: [
         {
@@ -61,7 +71,7 @@ const App = {
           skills: ["Pug", "SASS", "jQuery"],
           webUrl: "https://eov.mangot5.com/eov/index",
           githubUrl: "https://github.com/feliciaho/eov",
-          img : "/images/eovBanner.png",
+          img : "images/eovBanner.png",
         },
         {
           type: "sideProjects",
@@ -69,12 +79,19 @@ const App = {
           skills: ["HTML", "SCSS", "jQuery" ,"RESTful API"],
           webUrl: "https://go.digeam.com/event/20231220/",
           githubUrl: "https://github.com/feliciaho/eov",
-          img : "/images/ghostBanner.png",
+          img : "images/ghostBanner.png",
         },
       ],
     };
   },
   methods:{
+    scrollToTop(page) {
+      // 滾動到指定的頁面
+      const element = document.querySelector(page);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
   },
 };
 Vue.createApp(App).mount("#app");
