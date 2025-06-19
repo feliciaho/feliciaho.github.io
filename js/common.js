d7c91c4852818cc7c7ea17473c59f2ae5bd9bc1c
unknown <felicia18felicia18@gmail.com>
2025-06-19 13:34:22 +0800

    add homestay web

--- js/common.js
+++ js/common.js
@@ -2,13 +2,13 @@
 const App = {
   data() {
     return {
-      expriences:[
+      expriences: [
         {
           type: "Frontend Designer",
           name: "Happytuk",
           time: "April, 2024~April, 2025",
           skills: ["Pug", "SASS", "jQuery", "Gulp", "CICD", "Vue", "Git"],
-          list:[
+          list: [
             "負責開發並維護官網及活動頁，使用 Pug、SASS 和 JavaScript，jQuery 實現響應式設計和互動功能。",
             "使用 Vue 框架，協助公司將遊戲活動頁面從 JavaScript、jQuery 過渡到 Vue 開發模式。",
             "使用 Gulp 進行前端自動化工具，提升開發效率。",
@@ -20,8 +20,16 @@ const App = {
           type: "Website Designer",
           name: "Digeam",
           time: "April, 2023~April, 2024",
-          skills: ["HTML", "SCSS", "jQuery", "javascript", "RESTful API", "Vue", "Git"],
-          list:[
+          skills: [
+            "HTML",
+            "SCSS",
+            "jQuery",
+            "javascript",
+            "RESTful API",
+            "Vue",
+            "Git",
+          ],
+          list: [
             "負責開發並維護官網及活動頁，使用 HTML、SCSS 和 JavaScript，jQuery 實現響應式設計和互動功能。",
             "引入並整合 Vue 框架，協助公司從 JavaScript、jQuery 過渡到 Vue 開發模式。",
             "RESTful API 串接，實現與後端系統的數據交互和通知。",
@@ -33,18 +41,18 @@ const App = {
         {
           type: "Cabel",
           name: "Activity website",
-          skills: ["HTML", "SASS", "javascript", "Vue" ,"RESTful API"],
+          skills: ["HTML", "SASS", "javascript", "Vue", "RESTful API"],
           webUrl: "https://feliciaho.github.io/cbo_20240129",
           githubUrl: "https://github.com/feliciaho/cbo_20240129",
-          img : "images/cboBanner.png",
+          img: "images/cboBanner.png",
         },
         {
           type: "Ghost",
           name: "Christmas website",
-          skills: ["HTML", "SCSS", "jQuery" ,"RESTful API"],
+          skills: ["HTML", "SCSS", "jQuery", "RESTful API"],
           webUrl: "https://feliciaho.github.io/ghost_20231220",
           githubUrl: "https://github.com/feliciaho/ghost_20231220",
-          img : "images/ghostBanner.png",
+          img: "images/ghostBanner.png",
         },
         {
           type: "EOV",
@@ -52,26 +60,46 @@ const App = {
           skills: ["Pug", "SASS", "jQuery", "Gulp", "CICD"],
           webUrl: "https://eov.mangot5.com/eov/index",
           githubUrl: "https://github.com/feliciaho/eov",
-          img : "images/eovBanner.png",
+          img: "images/eovBanner.png",
         },
         {
           type: "LostArk",
           name: "Info website",
-          skills: ["Pug", "SASS", "jQuery","Vue", "Gulp", "CICD"],
-          webUrl: "https://landing.mangot5.com/template/lostark/event/250327_pletze/index.html",
+          skills: ["Pug", "SASS", "jQuery", "Vue", "Gulp", "CICD"],
+          webUrl:
+            "https://landing.mangot5.com/template/lostark/event/250327_pletze/index.html",
           githubUrl: "https://github.com/feliciaho/250327_pletze",
-          img : "images/lostarkBanner.jpg",
+          img: "images/lostarkBanner.jpg",
         },
-
       ],
       sideProjects: [
         {
           type: "sideProjects",
           name: "Recipe website",
-          skills: ["HTML", "SASS", "javascript" ,"RESTful API", "Vue", "Vite"],
+          skills: ["HTML", "SASS", "javascript", "RESTful API", "Vue", "Vite"],
           webUrl: "https://feliciaho.github.io/recipeWeb_new/#/homeView",
           githubUrl: "https://github.com/feliciaho/recipeWeb_new",
-          img : "images/recipeBanner.jpg",
+          figma: false,
+          img: "images/recipeBanner.jpg",
+        },
+        {
+          type: "sideProjects",
+          name: "Homestay website",
+          skills: [
+            "Figma",
+            "Figma MCP to Crusor",
+            "HTML",
+            "SASS",
+            "javascript",
+            "RESTful API",
+            "Vue",
+            "Vite",
+          ],
+          webUrl: "https://feliciaho.github.io/bookingWeb/#/homeView",
+          githubUrl: "https://github.com/feliciaho/bookingWeb",
+          figma: true,
+          figmaUrl: "https://www.figma.com/design/IWVbELJiG1oS9CLWX8PUBx/Hotel-Booking-Web?node-id=0-1&p=f",
+          img: "images/bookingBanner.jpg",
         },
       ],
       uiux: [
@@ -79,7 +107,7 @@ const App = {
           type: "Cabel",
           name: "Official website",
           webUrl: "https://cbo.digeam.com/index",
-          img : [
+          img: [
             "images/cabelOfficial/1.jpg",
             "images/cabelOfficial/2.jpg",
             "images/cabelOfficial/3.jpg",
@@ -89,31 +117,23 @@ const App = {
           type: "Cabel",
           name: "Activity website",
           webUrl: "https://cbo.digeam.com/OBT",
-          img : [
-            "images/cabelActivity/1.jpg",
-          ],
+          img: ["images/cabelActivity/1.jpg"],
         },
         {
           type: "Ghost",
           name: "Christmas website",
           webUrl: "https://feliciaho.github.io/ghost_20231220",
-          img : [
-            "images/ghost/1.jpg",
-            "images/ghost/2.jpg",
-          ],
+          img: ["images/ghost/1.jpg", "images/ghost/2.jpg"],
         },
         {
           type: "XX2",
           name: "New Character website",
-          img : [
-            "images/xx2/1.jpg",
-            "images/xx2/2.jpg",
-          ],
+          img: ["images/xx2/1.jpg", "images/xx2/2.jpg"],
         },
         {
           type: "flyff",
           name: "2nd Anniversary website",
-          img : [
+          img: [
             "images/flyff/1.jpg",
             "images/flyff/2.jpg",
             "images/flyff/3.jpg",
@@ -122,16 +142,16 @@ const App = {
       ],
     };
   },
-  methods:{
+  methods: {
     scrollToTop(page) {
       // 滾動到指定的頁面
       const element = document.querySelector(page);
       if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
+        element.scrollIntoView({ behavior: "smooth" });
       }
-    }
+    },
   },
-  mounted(){
+  mounted() {
     const swiper1 = new Swiper(".mySwiper1", {
       slidesPerView: 1,
       spaceBetween: 30,
@@ -151,24 +171,23 @@ const App = {
       centeredSlides: true,
       slidesPerView: "auto",
       coverflowEffect: {
-      rotate: 30,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-      scale: 0.7,
+        rotate: 30,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+        scale: 0.7,
       },
       pagination: {
-      el: ".swiper-pagination",
-      clickable: true, // Corrected from "click" to "clickable"
+        el: ".swiper-pagination",
+        clickable: true, // Corrected from "click" to "clickable"
       },
       autoplay: {
-      delay: 1500,
-      disableOnInteraction: false,
+        delay: 1500,
+        disableOnInteraction: false,
       },
       loop: true,
     });
   },
-  
 };
 Vue.createApp(App).mount("#app");
